b5cfd43542ea13f544bffe8ba02e57a310229d8b
Henry <hmorenosifontes@gmail.com>
2022-06-02 23:17:19 -0400

    conectando con blockchain ganache

--- contracts/TasksContract.sol
+++ contracts/TasksContract.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-contract TaskContract {
+contract TasksContract {
+
+uint taskCounter = 0;
+
 
 struct Task {
     uint256 id;
@@ -14,7 +17,8 @@ struct Task {
 mapping (uint256 =>  Task)  public tasks;
 
 function createTask(string memory _title,string memory _description) public  {
-
+    tasks[taskCounter] = Task(taskCounter, _title, _description, false, block.timestamp);
+    taskCounter++;
 }
 
 
